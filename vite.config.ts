38120d0377ddd517ee348081a6681d8c44e70661
Natalie <natalieaubrey44@gmail.com>
2026-03-22 08:18:20 +0100

    kiwi

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,16 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 
-export default defineConfig(() => {
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, '.', '');
   return {
     root: '.',
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
-    },
-    build: {
-      rollupOptions: {
-        input: {
-          index: path.resolve(__dirname, 'index.html'),
-        },
+        '@': path.resolve(__dirname, '.'),
       },
     },
     server: {
